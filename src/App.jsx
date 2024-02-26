1e857d00b881c72c9a62c752640b24204592adfc
yeasinshakil <shakilrock1998@gmail.com>
2024-02-26 22:44:48 +0600

    partners logo component add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import PartnersLogo from './components/PartnersLogo'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Header />
+      <PartnersLogo />
     </>
   )
 }
