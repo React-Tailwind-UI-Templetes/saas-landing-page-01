70ec5c61dc2a9f7802720fb2a44f2897be4bce83
yeasinshakil <shakilrock1998@gmail.com>
2024-02-27 18:05:13 +0600

    register cta section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import PartnersLogo from './components/PartnersLogo'
 import PaymentInfo from './components/PaymentInfo'
+import RegisterCTA from './components/RegisterCTA'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <PartnersLogo />
       <PaymentInfo />
+      <RegisterCTA />
     </>
   )
 }
