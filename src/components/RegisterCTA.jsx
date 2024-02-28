e530a14695285824ed752c2c84f0de731f8b286e
yeasinshakil <shakilrock1998@gmail.com>
2024-02-28 23:18:16 +0600

    register respnsive

--- src/components/RegisterCTA.jsx
+++ src/components/RegisterCTA.jsx
@@ -1,18 +1,18 @@
 import birds from '../assets/birds.png'
 const RegisterCTA = () => {
     return (
-        <div className="container my-20">
-            <div className="bg-[#F3F7FA] flex justify-between items-center gap-10 px-28 py-10 rounded-lg relative">
+        <div className="container xl:my-20">
+            <div className="bg-[#F3F7FA] flex flex-col lg:flex-row justify-between items-center gap-4 md:gap-6 xl:gap-10 px-2 md:px-4 xl:px-28 py-4 md:py-6 xl:py-10 rounded-lg relative">
                 <div className="absolute -top-8 right-0">
-                    <img src={birds} alt="" className='w-auto' />
+                    <img src={birds} alt="birds" className='w-auto' />
                 </div>
-                <div className="w-8/12">
-                    <p className='uppercase text-purple-800 text-xl py-6 font-semibold'>Feature</p>
-                    <h1 className='text-5xl font-bold tracking-wide py-8'>Receive payments quickly from anywhere</h1>
-                    <p className='text-xl py-10'>Why kept very ever home mrs. Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to. </p>
+                <div className="lg:w-8/12">
+                    <p className='uppercase text-purple-800 text-xl py-2 md:py-4 xl:py-6 font-semibold'>Feature</p>
+                    <h1 className='text-2xl sm:text-3xl xl:text-5xl font-bold tracking-wide py-3 sm:py-4 xl:py-8'>Receive payments quickly from anywhere</h1>
+                    <p className='text-xl xl:py-10'>Why kept very ever home mrs. Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to. </p>
 
                 </div>
-                <div className="w-1/3">
+                <div className="lg:w-1/3">
                     <form className="">
                         <p className="text-2xl text-orange-500 font-semibold">Get Started for Free</p>
                         <input type="text" placeholder="Enter your Email" className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-10" />
