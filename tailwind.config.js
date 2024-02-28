8f697d9f7e038c59ff834d80642ca5f887340570
yeasinshakil <shakilrock1998@gmail.com>
2024-02-28 22:54:00 +0600

    tailwind config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ export default {
     extend: {
       container: {
         center: true,
-        padding: '3rem'
+        padding: {
+          sm: '1rem',
+          md: '1rem',
+          lg: '1.5rem',
+          xl: '2rem',
+          '2xl': '4rem',
+        }
       },
 
     },
